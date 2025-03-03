0f5912b6b5a8314183f9bcfc2bbc0707548112ea
filiptrifunovski88 <58521444+filiptrifunovski88@users.noreply.github.com>
2025-03-03 15:42:16 +0100

    Add files via upload

---
+++ homework1/AverageNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AverageNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstInput = Console.ReadLine();
+            double firstNumber = double.Parse(firstInput);
+            Console.WriteLine("First number: " + firstNumber);
+
+            string secondInput = Console.ReadLine();
+            double secondNumber = double.Parse(secondInput);
+            Console.WriteLine("Second number: " + secondNumber);
+
+            string thirdInput = Console.ReadLine();
+            double thirdNumber = double.Parse(thirdInput);
+            Console.WriteLine("Third number: " + thirdNumber);
+
+            string fourthInput = Console.ReadLine();
+            double fourthNumber = double.Parse(fourthInput);
+            Console.WriteLine("Fourth number: " + fourthNumber);
+
+            double allNumbers = firstNumber + secondNumber + thirdNumber + fourthNumber;
+
+            Console.WriteLine("Average Number is: " + allNumbers / 4);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework1/SwapNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstInput = Console.ReadLine();
+            int firstNumber = int.Parse(firstInput);
+            Console.WriteLine("First number:" + firstNumber);
+
+            string secondInput = Console.ReadLine();
+            int secondNumber = int.Parse(secondInput);
+            Console.WriteLine("Second number: " + secondNumber);
+
+            int thirdNumber = default;
+            int fourthNumber = default;
+
+            thirdNumber = secondNumber;
+            fourthNumber = firstNumber;
+
+            Console.WriteLine("Switched first number: " + thirdNumber);
+            Console.WriteLine("Switched second number: " + fourthNumber);
+            Console.ReadLine();
+        }
+    }
+}
