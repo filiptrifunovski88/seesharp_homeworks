1d9dcee0ceab2ae9947fcd0a923e72972bd67695
filiptrifunovski88 <58521444+filiptrifunovski88@users.noreply.github.com>
2025-03-03 15:47:14 +0100

    Add files via upload

---
+++ homework1/RealCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace RealCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstInput = Console.ReadLine();
+            int firstNumber = int.Parse(firstInput);
+            Console.WriteLine(firstNumber);
+
+            string seconInput = Console.ReadLine();
+            int secondNumber = int.Parse(seconInput);
+            Console.WriteLine(secondNumber);
+
+            string thirdInput = Console.ReadLine();
+            Console.WriteLine(thirdInput);
+
+            if (thirdInput == "+")
+            {
+                Console.WriteLine(firstNumber + secondNumber);
+            }
+            else if (thirdInput == "-") {
+                Console.WriteLine(firstNumber - secondNumber);
+            }
+            else if (thirdInput == "/")
+            {
+                Console.WriteLine(firstNumber / secondNumber);
+            }
+            else if (thirdInput == "*")
+            {
+                Console.WriteLine(firstNumber * secondNumber);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
