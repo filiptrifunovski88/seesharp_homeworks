226d46801b8d5e6bcbf83f1a77289646d01eb410
Filip Trifunovski <filiptrifunovski88@gmail.com>
2025-03-10 03:09:28 +0100

    homework2

---
+++ homework2/StudentGroup.app/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace StudentGroup.app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] studentsG1 = new string[5];
+            string[] studentsG2 = new string[5];
+            
+            for (int i = 0; i < studentsG1.Length; i++)
+            {
+                Console.Write("Enter name number " + (i + 1) + " for the FIRST array: ");
+                studentsG1[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < studentsG2.Length; i++)
+            {
+                Console.Write("Enter name number " + (i + 1) + " for the SECOND array: ");
+                studentsG2[i] = Console.ReadLine();
+            }
+
+            string input = default;
+            bool isValidInput = false;
+            while (!isValidInput)
+            {
+                Console.Write("Enter number between 1 and 2: ");
+                input = Console.ReadLine();
+                if (input == "1" || input == "2")
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid number! Please enter 1 or 2.");
+            }
+
+            if (input == "1")
+            {
+                for (int i = 0; i < studentsG1.Length; i++)
+                {
+                    Console.WriteLine("Student in group 1: " + studentsG1[i] + " ");
+                }
+            }
+            else if (input == "2")
+            {
+                for (int i = 0; i < studentsG2.Length; i++)
+                {
+                    Console.WriteLine("Student in group 2: " + studentsG2[i] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ homework2/sumOfEven.app/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sumOfEven.app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sixNumberArr = new int[6];
+            int sum = 0;
+
+            for(int i = 0; i < sixNumberArr.Length; i++)
+            {
+                Console.Write("Enter " + (i + 1) + " number in the array: ");
+                sixNumberArr[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = 0; i < sixNumberArr.Length; i++)
+            {
+                if (sixNumberArr[i] % 2 == 0)
+                {
+                    sum += sixNumberArr[i];
+                }
+            }
+            Console.WriteLine("The sum of even numbers is: " + sum);
+        }
+    }
+}
