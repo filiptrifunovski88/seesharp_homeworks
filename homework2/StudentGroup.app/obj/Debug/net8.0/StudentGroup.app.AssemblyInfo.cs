f8d8c3ab75ecb30b3f1149c836ff036414e18702
Filip Trifunovski <filiptrifunovski88@gmail.com>
2025-03-10 03:37:00 +0100

    homework2

--- homework2/StudentGroup.app/obj/Debug/net8.0/StudentGroup.app.AssemblyInfo.cs
+++ homework2/StudentGroup.app/obj/Debug/net8.0/StudentGroup.app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGroup.app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+226d46801b8d5e6bcbf83f1a77289646d01eb410")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6c457ff977c3f736212da16645456a1f656bbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGroup.app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGroup.app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
