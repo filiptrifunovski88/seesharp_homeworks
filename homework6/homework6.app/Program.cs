b1515f2600ab21800fca8a48205650f69c44980f
Filip Trifunovski <filiptrifunovski88@gmail.com>
2025-03-21 14:33:50 +0100

    homework6

---
+++ homework6/homework6.app/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace homework6.app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> listOfNumbers = new Queue<int>();
+
+            while(true)
+            {
+                Console.WriteLine("Enter number: ");
+                string input = Console.ReadLine();
+
+                if(int.TryParse(input, out int convertedInput))
+                {
+                    listOfNumbers.Enqueue(convertedInput);
+                }
+                else
+                {
+                    Console.WriteLine("Enter valid number!");
+                    continue;
+                }
+
+                Console.WriteLine("If you want to add more numbers write ( Y - yes ) or ( N - no )");
+                string input2 = Console.ReadLine().ToUpper();
+
+                if(input2 != "Y")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Number/s you entered: ");
+            foreach(int number in listOfNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
